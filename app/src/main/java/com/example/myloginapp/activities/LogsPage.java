c8a15e7459af050f69398264a97ca20d1dc51c7f
creich21 <creich21@student.aau.dk>
2023-04-25 13:21:23 +0200

    Log Recyclerview works :)

--- app/src/main/java/com/example/myloginapp/activities/LogsPage.java
+++ app/src/main/java/com/example/myloginapp/activities/LogsPage.java
@@ -10,6 +10,8 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.myloginapp.R;
+import com.example.myloginapp.adapters.LogAdapter;
+import com.example.myloginapp.adapters.URP_RecyclerViewAdapter;
 import com.example.myloginapp.models.CurrentUser;
 import com.example.myloginapp.models.LogsModel;
 import com.example.myloginapp.models.UserRightsModel;
@@ -21,11 +23,14 @@ import com.google.android.material.navigation.NavigationView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
@@ -38,7 +43,7 @@ public class LogsPage extends AppCompatActivity implements NavigationView.OnNavi
     String urlStr;
     String[] actions = {"change user rights","redeem","distribute"};
     CurrentUser currentUser;
-
+    ArrayList<LogsModel> logsModels = new ArrayList<>();
 
 
     @Override
@@ -49,13 +54,13 @@ public class LogsPage extends AppCompatActivity implements NavigationView.OnNavi
 
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.activity_logs_page);
 
         /*------------------------Hooks----------------------------------------*/
-        drawerLayout = findViewById(R.id.drawerHomePage);
+        drawerLayout = findViewById(R.id.drawerLogsPage);
         navigationView = findViewById(R.id.navigationView);
 
-        toolbar = findViewById(R.id.home_page_toolbar);
+        toolbar = findViewById(R.id.logs_page_toolbar);
         navigationView.setItemIconTintList(null);
 
         /*------------------------Toolbar----------------------------------------*/
@@ -71,6 +76,17 @@ public class LogsPage extends AppCompatActivity implements NavigationView.OnNavi
 
         /*----------------------Get Logs------------------------------------------*/
 
+
+        /*------------------------Recycler View----------------------------*/
+        RecyclerView recyclerView = findViewById(R.id.logRecyclerView);
+
+        setUpLogsModels();
+
+        LogAdapter adapter = new LogAdapter(this, logsModels);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+    private void setUpLogsModels() {
         token = tokenManager.getJwtToken();
 
 
@@ -81,7 +97,7 @@ public class LogsPage extends AppCompatActivity implements NavigationView.OnNavi
         data.put("action",actions.toString());
         ReqObj obj = new ReqObj(data);
 
-        if(currentUser.getRole() == "director") {
+        if(Objects.equals(currentUser.getRole(), "director")) {
             urlStr = "director/log";
         } else {
             urlStr = "account/log";
@@ -94,10 +110,26 @@ public class LogsPage extends AppCompatActivity implements NavigationView.OnNavi
             // The response objects 'data'-field contains an array with objects. One for each user
             // containing the username and the role of the user.
             // It is unpacked like this:
+
+
+
             ArrayList<Map<String, Object>> dataMap = (ArrayList<Map<String, Object>>) resMap.get("response"); // Get the array from 'data'-field. Output: [{username: , role: },{},... ]
 
             // Goes through the items in dataMap, which is an array of Map-objects, to store
             // their data in a UserRightsModel, so that it can be used by the recycler view.
+            System.out.println(dataMap);
+            Integer i = 0;
+            assert dataMap != null;
+            for (Map<String, Object> dataObj : dataMap) {
+                System.out.print("create object");
+                System.out.println(i);
+                logsModels.add(new LogsModel(dataObj));
+                i++;
+            }
+
+
+
+
 
 
         } catch (Exception e) {
@@ -117,6 +149,9 @@ public class LogsPage extends AppCompatActivity implements NavigationView.OnNavi
             super.onBackPressed();
         }
     }
+
+
+
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
--- app/src/main/java/com/example/myloginapp/adapters/LogAdapter.java
+++ app/src/main/java/com/example/myloginapp/adapters/LogAdapter.java
@@ -4,70 +4,193 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.myloginapp.R;
 import com.example.myloginapp.models.LogsModel;
 
 import java.util.ArrayList;
 
-public class LogAdapter extends RecyclerView.Adapter<com.example.myloginapp.adapters.LogAdapter.MyViewHolder> {
-
-
+public class LogAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     Context context;
-    ArrayList<LogsModel> logModels;
 
-    public LogAdapter(Context context, ArrayList<LogsModel> logModels) {
+    public LogAdapter(Context context, ArrayList<LogsModel> logsModels) {
         this.context = context;
-        this.logModels = logModels;
+        this.logsModels = logsModels;
+        System.out.println("constructor");
     }
 
-    @NonNull
-    @Override
-    public com.example.myloginapp.adapters.LogAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        // This method inflates the layout, i.e. gives it its look.
-        LayoutInflater inflater = LayoutInflater.from(context);
-        View view = inflater.inflate(R.layout.recycler_view_logs_user_role, parent, false);
+    ArrayList<LogsModel> logsModels;
 
-        return new com.example.myloginapp.adapters.LogAdapter.MyViewHolder(view);
-    }
+    private static final int VIEW_TYPE_ONE = 1;
+    private static final int VIEW_TYPE_TWO = 2;
+    private static final int VIEW_TYPE_THREE = 3;
+
 
     @Override
-    public void onBindViewHolder(@NonNull com.example.myloginapp.adapters.LogAdapter.MyViewHolder holder, int position) {
-        // This method assigns values to the views in the recycler_view_row layout file,
-        // based on the position of the recycler view.
-        holder.textView.setText(logModels.get(position).getUsername());
-        holder.button.setText(logModels.get(position).getRole());
-
-        // Below set the profile picture. Ignore for now
-        // holder.imageView.setImageResource(userRightsModels.get(position).getImage());
+    public int getItemViewType(int position) {
+        // Determine the view type based on the "action" property of the object at this position
+        LogsModel logObj = logsModels.get(position);
+        String action = logObj.getAction();
+        System.out.println("getItemViewType");
+        if (action.equals("change user rights")) {
+            return VIEW_TYPE_ONE;
+        } else if (action.equals("distribute")) {
+            return VIEW_TYPE_TWO;
+        } else if (action.equals("redeem")) {
+            return VIEW_TYPE_THREE;
+        } else {
+            return -1;
+        }
     }
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Inflate the appropriate layout based on the view type
+        System.out.println("onCreateViewHolder");
+        switch (viewType) {
+            case VIEW_TYPE_ONE:
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_logs_user_role, parent, false);
+                return new ChangeUserRightsViewHolder(view);
+            case VIEW_TYPE_TWO:
+                View view2 = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_logs_dist, parent, false);
+                return new DistributeViewHolder(view2);
+            case VIEW_TYPE_THREE:
+                View view3 = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_logs_redeem, parent, false);
+                return new RedeemViewHolder(view3);
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        LogsModel logObj = logsModels.get(position);
+        System.out.println("onBindViewHolder");
+        System.out.println(holder.getItemViewType());
+        switch (holder.getItemViewType()) {
+            case VIEW_TYPE_ONE:
+                System.out.println("case1");
+                ChangeUserRightsViewHolder changeUserRightsViewHolder = (ChangeUserRightsViewHolder) holder;
+                changeUserRightsViewHolder.bind(logObj);
+                break;
+            case VIEW_TYPE_TWO:
+                System.out.println("case2");
+                DistributeViewHolder distributeViewHolder = (DistributeViewHolder) holder;
+                distributeViewHolder.bind(logObj);
+                break;
+            case VIEW_TYPE_THREE:
+                System.out.println("case3");
+                RedeemViewHolder redeemViewHolder = (RedeemViewHolder) holder;
+                redeemViewHolder.bind(logObj);
+                break;
+        }
+
+
+    }
+
 
     @Override
     public int getItemCount() {
-        // This method tells the recyclerView how many items it has to display.
-        return logModels.size();
+        /*System.out.println("getItemCount")*/;
+        return logsModels.size();
     }
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
-        //ImageView imageView; // Will eventually hold the profile picture.
-        TextView textView;
-        Button button;
+    private static class ChangeUserRightsViewHolder extends RecyclerView.ViewHolder {
+        // Define the views for this view holder
 
-        public MyViewHolder(@NonNull View itemView) {
+        TextView user;
+        TextView action;
+        TextView date;
+        TextView receiverField;
+        TextView roleField;
+        public ChangeUserRightsViewHolder(View itemView) {
             super(itemView);
 
-            //imageView = itemView.findViewById(R.id.imageView);
-            textView = itemView.findViewById(R.id.textView);
-            button = itemView.findViewById(R.id.button);
+            user = itemView.findViewById(R.id.logInitiator);
+            action = itemView.findViewById(R.id.logAction);
+            date = itemView.findViewById(R.id.logDate);
+            receiverField = itemView.findViewById(R.id.logReceiverField);
+            roleField = itemView.findViewById(R.id.logRoleField);
+            System.out.println("ChangeUserRightViewHolder Constructor");
+        }
+
+        public void bind(LogsModel logObj) {
+            user.setText(logObj.getInitiator());
+            action.setText(logObj.getAction());
+            date.setText(logObj.getDate());
+            receiverField.setText(logObj.getReceiver());
+            roleField.setText(logObj.getRole());
+
+            System.out.println("ChangeUserRightViewHolder Bind");
+
         }
 
     }
 
+    private static class DistributeViewHolder extends RecyclerView.ViewHolder {
+        TextView user;
+        TextView action;
+        TextView date;
+        TextView receiverField;
+        TextView tokensCount;
 
+
+        public DistributeViewHolder(View itemView) {
+            super(itemView);
+
+            user = itemView.findViewById(R.id.logInitiator);
+            action = itemView.findViewById(R.id.logAction);
+            date = itemView.findViewById(R.id.logDate);
+            receiverField= itemView.findViewById(R.id.logReceiverField);
+            tokensCount = itemView.findViewById(R.id.logTokensCount);
+
+            System.out.println("Distribute Constructor");
+
+
+            /*tokensInt = itemView.findViewById(R.id.logTokensInt);*/
+        }
+
+        public void bind(LogsModel logObj) {
+            user.setText(logObj.getInitiator());
+            action.setText(logObj.getAction());
+            date.setText(logObj.getDate());
+            receiverField.setText(logObj.getReceiver());
+            tokensCount.setText(logObj.getTokensCount());
+            System.out.println("Distribute Bind");
+
+        }
+    }
+
+    private static class RedeemViewHolder extends RecyclerView.ViewHolder {
+        // Define the views for this view holder
+        TextView user;
+        TextView action;
+        TextView date;
+
+        public RedeemViewHolder(View itemView) {
+            super(itemView);
+
+            user = itemView.findViewById(R.id.logInitiator);
+            action = itemView.findViewById(R.id.logAction);
+            date = itemView.findViewById(R.id.logDate);
+
+            /*System.out.println("ChangeUserRightRedeem Constructor");*/
+
+        }
+
+        public void bind(LogsModel logObj) {
+            user.setText(logObj.getInitiator());
+            action.setText(logObj.getAction());
+            date.setText(logObj.getDate());
+
+            System.out.println("ChangeUserRightRedeem Bind");
+
+        }
+    }
 }
+
